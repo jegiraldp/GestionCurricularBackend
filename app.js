9a4414551bbfe3ba5583f08791887f162e5020ee
JuanPrat <33468928+JuanPrat@users.noreply.github.com>
2022-12-06 23:38:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -36,6 +36,13 @@ var corsOptions = {
 }
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 router.use('/', userRoute);
 router.use('/auth/', authRoute);
